802b61af5dca6f5bfb6239d4137781f9cb5f11c4
Tom Adams <tom@holizz.com>
2010-11-29 12:02:19 -0500

    Assignment works.

--- hp.py
+++ hp.py
@@ -4,17 +4,43 @@ import ast
 
 class HotPotato:
     def __init__(self, fn):
-        self.ast = compile(open('test.py').read(),'test.py','exec', ast.PyCF_ONLY_AST)
+        self.ast = compile(open('test.py').read(),
+                'test.py',
+                'exec',
+                ast.PyCF_ONLY_AST)
+
     def php(self):
         return self._php(self.ast)
+
     def _php(self, a):
-        if a.__class__ == ast.Module:
+        b = a.__class__
+        if b == ast.Module:
             s = ''
             for b in a.body:
                 s += self._php(b)
+                s += ';\n'
             return s
+
+        elif b == ast.Assign:
+            return self._php(a.targets[0]) + ' = ' + self._php(a.value)
+
+        elif b == ast.Name:
+            return '$'+a.id
+
+        elif b == ast.List:
+            s = 'array( '
+            s += ' , '.join([self._php(e) for e in a.elts])
+            s += ' )'
+            return s
+
+        elif b == ast.Num:
+            return str(a.n)
+
+        elif b == ast.Str:
+            return "'" + a.s + "'"
+
         else:
-            return 'UNRECOGNISED'
+            return repr(a.__class__)
 
 hp = HotPotato('test.py')
 print(hp.php())
