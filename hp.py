286c3efaaa4d98e9169ef424b094b3c69d4c8500
Tom Adams <tom@holizz.com>
2010-11-29 12:18:00 -0500

    Demo data works.

--- hp.py
+++ hp.py
@@ -10,34 +10,54 @@ class HotPotato:
                 ast.PyCF_ONLY_AST)
 
     def php(self):
-        return self._php(self.ast)
+        return '<?php\n'+self._php(self.ast)
 
     def _php(self, a):
-        b = a.__class__
-        if b == ast.Module:
+        c = a.__class__
+        if c == ast.Module:
             s = ''
             for b in a.body:
                 s += self._php(b)
-                s += ';\n'
+                s += '\n'
             return s
 
-        elif b == ast.Assign:
-            return self._php(a.targets[0]) + ' = ' + self._php(a.value)
+        elif c == ast.Assign:
+            return self._php(a.targets[0]) + ' = ' + self._php(a.value) + ';'
 
-        elif b == ast.Name:
+        elif c == ast.Name:
             return '$'+a.id
 
-        elif b == ast.List:
+        elif c == ast.List:
             s = 'array( '
             s += ' , '.join([self._php(e) for e in a.elts])
             s += ' )'
             return s
 
-        elif b == ast.Num:
+        elif c == ast.Num:
             return str(a.n)
 
-        elif b == ast.Str:
+        elif c == ast.Str:
             return "'" + a.s + "'"
+
+        elif c == ast.For:
+            s = 'foreach ( '
+            s += self._php(a.iter)
+            s +=' as '
+            s += self._php(a.target)
+            s += ') {\n'
+            s += ';\n'.join([self._php(b) for b in a.body])
+            s += '}'
+            return s
+
+        elif c == ast.Expr:
+            return self._php(a.value) + ';\n'
+
+        elif c == ast.Call:
+            s = a.func.id
+            s += '( '
+            s += ' , '.join([self._php(b) for b in a.args])
+            s += ' )'
+            return s
 
         else:
             return repr(a.__class__)
